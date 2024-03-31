a3a06a4365ba572c59576f9cd5ef936cea1d93fc
lioritah <lioritah@gmail.com>
2024-03-31 03:28:35 +0300

    first commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import bambooProjectThumbnail from "../../../assets/Bamboo social network.png";
-// import portfolioProjectThumbnail from "../../../assets/portfolio-thumbnail.png";
+import portfolioProjectThumbnail from "../../../assets/portfolio-thumbnail.png";
+import chatAppProjectThumbnail from "../../../assets/chatApp.png";
 import Carousel from "../Carousel/Carousel";
 
 import "./Projects.scss";
@@ -12,12 +13,18 @@ const projects = [
     description: "Social network app",
     thumbnail: bambooProjectThumbnail,
   },
-  // {
-  //   name: 'Portfolio',
-  //   url: 'https://bendevportfolio.onrender.com',
-  //   description: 'My portfolio website',
-  //   thumbnail: portfolioProjectThumbnail
-  // }
+  {
+    name: "Portfolio",
+    url: "https://lioritah-portfolio.netlify.app",
+    description: "My portfolio website",
+    thumbnail: portfolioProjectThumbnail,
+  },
+  {
+    name: "Chatgram",
+    url: "https://chatappmaster.netlify.app",
+    description: "Chat-app website",
+    thumbnail: chatAppProjectThumbnail,
+  },
 ];
 
 export default function Projects() {
