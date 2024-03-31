e3c45d801d3a0ccb1a19e07b1f5e11188b5ad0f1
lioritah <lioritah@gmail.com>
2024-04-01 02:46:55 +0300

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,12 +13,12 @@ export default function About() {
             Who am I?
           </div>
           <p className="about-summery-desc paragraph-content">
-            A <strong>fullstack web developer</strong> with 5 years of
-            experience in software development. I'm currently looking for my
-            next challenge as a <strong>fullstack web developer</strong> in a
-            place where I can learn, grow and contribute. I have passion for
-            coding and learning new technologies, independent and{" "}
-            <strong>self learner</strong> with 'can do' attitude.
+            A <strong>fullstack web developer</strong> with the ability and
+            motivation to succeed. I'm currently looking for my next challenge
+            as a <strong>fullstack web developer</strong> in a place where I can
+            learn, grow and contribute. I have passion for coding and learning
+            new technologies, independent and <strong>self learner</strong> with
+            'can do' attitude.
           </p>
         </div>
 
